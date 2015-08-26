b88f8cba4bcdabb7d2ce0c7fe3f22104aa993e17
mickiesickie <miguelcisnerossalas@gmail.com>
2015-08-25 22:47:18 -0500

    funcionalidadTAbs

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,1 +1,12 @@
 
+ $( document ).ready(function() {
+    
+     $('.wrapperTabs .tab').on('click', function(event) {
+        event.preventDefault();
+        $('.wrapperTabs .tab').removeClass('tabActive');
+        $(this).addClass('tabActive');
+        var cual = $(this).data('tab');
+        $('.wrapperContentTab  .contentTab').removeClass('projectActive');
+        $('.wrapperContentTab .atributo' + cual).addClass('projectActive');
+    });
+});
