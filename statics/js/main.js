6d1aea627c3371b95aa6138245f8b3e182715d2f
mickiesickie <miguelcisnerossalas@gmail.com>
2015-08-27 00:17:08 -0500

    efectohover

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,12 +1,29 @@
 
+ 
  $( document ).ready(function() {
-    
+    //TABS PROYECTOS
      $('.wrapperTabs .tab').on('click', function(event) {
+     	//prevengo accion 
         event.preventDefault();
+        //REMUEVO CLASE POR SI YA FUE CLICKEADO 
         $('.wrapperTabs .tab').removeClass('tabActive');
+        //AGREGO CLASE A ESTE EN PARTICULAR
         $(this).addClass('tabActive');
+        //RECUEPERO VALOR DE DATA 
         var cual = $(this).data('tab');
+        //REMUEVO CLASE POR SI YA SE MOSTRO 
         $('.wrapperContentTab  .contentTab').removeClass('projectActive');
+        //PASO MI DATA PARA SABER CUAL MOSTRAR
         $('.wrapperContentTab .atributo' + cual).addClass('projectActive');
     });
+
+     //TAB VALORES NOSOTROS
+     $('#valores-nav .titleValores').on('click',function(event){
+     	$('#valores-nav .titleValores').removeClass('active');
+     	$(this).addClass('active');
+     	var queValor = $(this).data('rel');
+     	console.log(queValor);
+     	$('.wrapperValores .contenedorValores').removeClass('valorActive');
+     	$('.wrapperValores .valor' + queValor).addClass('valorActive');
+     });
 });
